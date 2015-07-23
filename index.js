951eff564b6a2ae40b9f4288d2a13a488fa17643
John Weis <weis.john@gmail.com>
2015-07-23 14:40:01 -0400

    initial implementation

---
+++ index.js
@@ -0,0 +1,38 @@
+var bcrypt = require('bcrypt');
+
+module.exports = function(schema, prop, config) {
+
+    if (!prop) throw new Error('prop must be specified');
+    
+    // token virtual references
+    var key = '_' + prop;
+    var hash_prop = 'hash' + key;
+
+    // bcrypt steps
+    var steps = config && config.steps || 10;
+
+    // extend the schema
+    var extension = {};
+    extension[hash_prop] = String;
+    schema.add(extension);
+
+    // setup access methods
+    schema.virtual(prop)
+        .set(function(value) {
+            this[key] = value;
+            // encrypt value
+            // TODO: # of steps should be config driven
+            var salt = bcrypt.genSaltSync(steps);
+            this[hash_prop] = bcrypt.hashSync(value, salt);
+        })
+        .get(function() { return this[key]; });
+
+    // abstract authentication function generator
+    schema.methods['verify_' + prop] = function(value, cb) {
+        if (!this[hash_prop]) return false;
+        if (typeof cb !== "function") 
+            return bcrypt.compareSync(value, this[hash_prop]);
+        bcrypt.compare(value, this[hash_prop], cb);
+    };
+
+}
